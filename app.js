e82275ca6d58e6a85cb358c1159644906fbb5105
Travis <travis.moulton002@gmail.com>
2021-12-13 09:19:40 -0500

    set up eslint. Also set up unhandled rejection error, sigterm, and uncaught exception handler

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // INIT the app
 const app = express();
 
 // Development logging
-if (process.send.NODE_ENV === "development") app.use(morgan("dev"));
+if (process.send.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // Set security http headers
 app.use(helmet());
--- server.js
+++ server.js
@@ -1,18 +1,40 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+process.openStdin('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!, Shutting down...');
+  console.log(err.name, err.message);
+  // On uncaught exceptions we need to terminate immediatley.
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 
 const { DATABASE, DB_PASSWORD } = process.env;
 
-const app = require("./app");
+const app = require('./app');
 
-const DB = DATABASE.replace("<password>", DB_PASSWORD);
+const DB = DATABASE.replace('<password>', DB_PASSWORD);
 
-mongoose.connect(DB).then(() => console.log("DB connection successful"));
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
   console.log(`App running on prt ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION, SHUTTING DOWN...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process Terminated');
+  });
 });
