edf43780d4215eb1aaa3092bab57890fb1e3c806
Travis <travis.moulton002@gmail.com>
2021-12-14 08:14:31 -0500

    added password hashing to the user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
-// const crypto = require('crypto');
+const crypto = require('crypto');
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -22,12 +22,56 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
   },
   createdAt: {
     type: Date,
     default: Date.now(),
   },
+  passwordChangedAt: Date,
 });
+
+// Hash password upon user creation and password update
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  // Only run the function when a user updates their password,
+  // Not when a new user is created
+  if (this.isModified('password') || this.isNew) return next();
+
+  // The database save is slower than creating the web token, so we adjust
+  // passwordChangeAt by 1 second so that the date is before the json
+  // web token is created
+  this.passwordChangedAt = Date.now() - 1000;
+});
+
+// Method to check that the given password matches is correct
+userSchema.methods.correctPassword = async function (
+  givenPassword,
+  userPassword
+) {
+  return await bcrypt.compare(givenPassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
