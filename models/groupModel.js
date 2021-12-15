c829a2601775008db8881208ae304be6e34f0a03
Travis <travis.moulton002@gmail.com>
2021-12-15 18:33:43 -0500

    initial schema

---
+++ models/groupModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A group must have a name'],
+    unique: true,
+  },
+  // members -> TBD
+  // Threads -> TBD
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  avatar: {
+    type: String,
+    default: 'defaultAvatar.jpg',
+  },
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
