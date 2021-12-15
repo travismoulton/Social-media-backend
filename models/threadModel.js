2e8f4876477bae9a23e8edec7592d978ee1a70f7
Travis <travis.moulton002@gmail.com>
2021-12-15 18:47:35 -0500

    initial schema for Thread

---
+++ models/threadModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const threadSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A thread must have a title'],
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  group: {
+    type: mongoose.Schema.ObjectId,
+    rerf: 'Group',
+  },
+  // Posts => Virtual populate
+  // Post count -> Siome sort of aggregation
+  // Like / Dislike score -> some sort of aggergation
+  // Contributors -> Not sure if needed yet.
+});
