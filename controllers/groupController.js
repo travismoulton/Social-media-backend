954779b4689b5a8dbac306ecb66685b119dde015
Travis <travis.moulton002@gmail.com>
2021-12-16 10:20:35 -0500

    in groupController, when creating a group, set up functionality to create a membership for the founding user

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,6 +1,20 @@
 const Group = require('../models/groupModel');
+const User = require('../models/userModel');
 // const AppError = require('../utils/appError');
-// const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.createGroup = factory.createOne(Group, 'Group');
+// exports.createGroup = factory.createOne(Group, 'Group');
+
+exports.createGroup = catchAsync(async (req, res, next) => {
+  const group = await Group.create(req.body);
+
+  // Create a membership for the foudning user
+  const user = await User.findById(req.user);
+  user.addGroupMembership(group._id);
+
+  res.status(201).json({
+    status: 'sucess',
+    data: { group },
+  });
+});
