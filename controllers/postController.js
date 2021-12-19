fabe0111035ef5a957c77a577f76c72ee1e585c2
Travis <travis.moulton002@gmail.com>
2021-12-19 16:51:45 -0500

    implemented dislike functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -34,7 +34,7 @@ exports.addLike = catchAsync(async (req, res, next) => {
     }
   );
 
-  const unlikedPost = await Post.findOneAndUpdate(
+  const neutralPost = await Post.findOneAndUpdate(
     {
       _id: postId,
       usersLiked: { $ne: userId },
@@ -45,13 +45,49 @@ exports.addLike = catchAsync(async (req, res, next) => {
     }
   );
 
-  if (!unlikedPost && !dislikedPost)
+  if (!neutralPost && !dislikedPost)
     return next(new AppError('This user has already liked this post', 400));
 
-  const data = unlikedPost ? { unlikedPost } : { dislikedPost };
+  const data = neutralPost ? { neutralPost } : { dislikedPost };
 
-  res.status(200).json({
-    status: 'Success',
-    data,
-  });
+  res.status(200).json({ status: 'Success', data });
+});
+
+exports.addDislike = catchAsync(async (req, res, next) => {
+  const postId = req.params.id;
+  const userId = req.user._id;
+
+  if (!(await Post.findById(postId)))
+    return next(new AppError('No post with that Id exists', 404));
+
+  const likedPost = await Post.findOneAndUpdate(
+    {
+      _id: postId,
+      usersLiked: { $eq: userId },
+      usersDisliked: { $ne: userId },
+    },
+    {
+      $inc: { likeCount: -1, dislikeCount: 1 },
+      $pull: { usersLiked: userId },
+      $push: { usersDisliked: userId },
+    }
+  );
+
+  const neutralPost = await Post.findOneAndUpdate(
+    {
+      _id: postId,
+      usersDisliked: { $ne: userId },
+    },
+    {
+      $inc: { likeCount: 1 },
+      $push: { usersDisliked: userId },
+    }
+  );
+
+  if (!neutralPost && !likedPost)
+    return next(new AppError('This user has already disliked this post', 404));
+
+  const data = neutralPost ? { neutralPost } : { likedPost };
+
+  res.status(200).json({ status: 'success', data });
 });
