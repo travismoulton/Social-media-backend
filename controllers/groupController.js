1a3adf748fb0b61648b0f11e64659b3b2bce4b09
Travis <travis.moulton002@gmail.com>
2022-01-13 09:45:24 -0500

    inside createGroup, added an error catch if the group name is already in use

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,8 +3,14 @@ const User = require('../models/userModel');
 // const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const sendErrorJson = require('../utils/sendErrorJson');
 
 exports.createGroup = catchAsync(async (req, res, next) => {
+  const existingGroup = await Group.find({ name: { $eq: req.body.name } });
+
+  if (existingGroup.length)
+    return sendErrorJson(res, 'A group already exists with that name', 404);
+
   const group = await Group.create({ ...req.body, createdBy: req.user });
 
   // Create a membership for the foudning user
