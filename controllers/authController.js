5286e8f52855fd1d10dd49a34dc045c6bedde65e
Travis <travis.moulton002@gmail.com>
2022-02-26 10:47:31 -0500

    removed trailing /

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const createAndSendToken = (user, statusCode, req, res) => {
     expires: new Date(Date.now() + ninetyDays),
     httpOnly: true,
     secure: true,
-    domain: '.social-app-frontend.netlify.app/',
+    domain: '.social-app-frontend.netlify.app',
   });
 
   // remove password from output
