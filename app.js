63796eade15455c932660fd91ecc0733fd09a15b
Travis <travis.moulton002@gmail.com>
2022-02-26 09:22:24 -0500

    switched CORS to new permanant Netlify route

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(
   cors({
     // origin: 'https://localhost:3000',
-    origin: 'https://621a3615af444b0a7a179eda--social-app-frontend.netlify.app',
+    origin: 'https://social-app-frontend.netlify.app',
     // origin: '*',
     credentials: true,
     exposedHeaders: ['set-cookie'],
